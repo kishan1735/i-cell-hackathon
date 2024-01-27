e0a5e3b12213fea4ed5d640d4e5aab86a1a2d478
skoriop <karthikp.ivy@gmail.com>
2024-01-28 04:53:10 +0530

    add timetable generate endpoint

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,32 +1,67 @@
-// @ts-nocheck
 import type { NextApiRequest, NextApiResponse } from "next";
 import { openai } from "@/lib/openai";
+import connectMongoDB from "@/lib/dbConnect";
+import CourseSet from "@/models/courseModel";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { NextResponse } from "next/server";
 
 type ResponseData = {
   message: string;
 };
 
-function isClash(timetable) {
+function isClash(timetable: any[]) {
   const set = new Set();
   for (const section of timetable) {
-    if (set.has(section.timings)) {
-      return false;
+    for (const timing of section.roomTime) {
+      const time = timing.split(":")[2] + timing.split(":")[3];
+      if (set.has(time)) {
+        return true;
+      }
+      set.add(time);
     }
-    set.add(section.timings);
   }
-  return true;
+  return false;
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
+export async function GET(req: NextApiRequest) {
+  // const session = await getServerSession(req, res, authOptions);
+  // if (!session) {
+  //   return res.status(401).json({ message: "Unauthorized" });
+  // }
   // get sections from DB
+  await connectMongoDB();
+  let courses = (
+    await CourseSet.findOne({
+      userEmail: "f20220022@hyderabad.bits-pilani.ac.in",
+    })
+  ).courses;
 
-  let courses = [];
+  let temp = [];
+  for (const course of courses) {
+    let arr = [[], [], []];
+    for (const section of course.sections) {
+      if (section.type == "L") {
+        arr[0].push(section);
+      } else if (section.type == "T") {
+        arr[1].push(section);
+      } else {
+        arr[2].push(section);
+      }
+    }
+    for (const a of arr) {
+      if (a.length !== 0) {
+        temp.push({
+          ...course,
+          sections: a,
+        });
+      }
+    }
+  }
+  courses = temp;
 
   let fin = [];
-  while (fin.size() < 50) {
+  while (fin.length < 5) {
     courses = courses
       .map((value) => ({ value, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
@@ -34,7 +69,7 @@ export default function handler(
 
     let prompt =
       "\
-        Task: Develop a timetable generator for college students that ensures no overlap in class timings. \
+        Task: Develop an optimal timetable generator for college students that ensures no overlap in class timings. \
         Requirements: \
         - There are 6 working days in a week: Monday (M), Tuesday (T), Wednesday (W), Thursday (Th), Friday (F), and Saturday (S). \
         - Each day has 11 hours, numbered from 1 to 11. \
@@ -50,30 +85,24 @@ export default function handler(
       prompt += `Course \'${course.name}\'\n`;
       for (const section of course.sections) {
         prompt += `Section \'${section.name}\'\n`;
-        for (const timing of section.timings) {
+        for (const timing of section.roomTime) {
           prompt += `${timing}\n`;
         }
       }
-      timetable.push(course[Math.floor(Math.random() * course.length)]);
-    }
-
-    const completion = await openai.completions.create({
-      messages: [{ role: "user", content: prompt }],
-      model: "gpt-3.5-turbo",
-    });
-    const res = completion.choices[0].text;
-
-    const regex = RegExp("Course '(.*)': Option (\\d\\d?)", "g");
-    const sectionMap = new Map();
-    while ((match = regex.exec(res)) !== null) {
-      console.log(
-        `Found ${match.groups[0]}. Next starts at ${regex.lastIndex}.`
+      timetable.push(
+        course.sections[Math.floor(Math.random() * course.sections.length)]
       );
-      sectionMap.set(match.groups[0], match.groups[1]);
     }
 
-    if (!isClash(timetable)) {
-      fin.push(timetable);
+    try {
+      console.log(timetable);
+      console.log(isClash(timetable));
+      if (!isClash(timetable)) {
+        fin.push(timetable);
+      }
+    } catch (err) {
+      console.log(err);
     }
   }
+  return NextResponse.json({ status: "success", data: fin });
 }
