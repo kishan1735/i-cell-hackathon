10a95b4b1f2179002628c49e6a87a37ffec648bf
skoriop <karthikp.ivy@gmail.com>
2024-01-28 03:49:25 +0530

    add avatar popup in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export function Navbar() {
   return (
@@ -8,10 +10,18 @@ export function Navbar() {
       <div>Saved Timetables</div>
       <div className="flex flex-row justify-end flex-grow px-4">
         {/* TODO: show user Google avatar */}
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="mt-2 mr-2 border-none w-32 flex flex-col gap-4 items-start bg-slate-700 text-slate-200">
+            <div>Profile</div>
+            <div>Logout</div>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
