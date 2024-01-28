af2f9aa43788b4d1d14f145e4bc7753192dcc6e8
skoriop <karthikp.ivy@gmail.com>
2024-01-28 08:31:05 +0530

    fix redirects in auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,35 @@
 "use client";
 import AuthCheck from "@/components/AuthCheck";
 import { ArrowRight } from "lucide-react";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 
 function Page() {
   const { data: session, status } = useSession();
   console.log(session);
   return (
-    <AuthCheck>
-      <div className="text-6xl w-5/6 flex flex-col justify-start h-[calc(100vh-200px)] pt-14 gap-5 text-white">
-        <div className="text-7xl text-white">chronoX</div>
-        <div className="text-2xl text-slate-500 pb-6">
-          Timetables - but supercharged
-        </div>
-        <Link
-          className="font-bold py-3 px-6 text-2xl flex gap-2 items-center text-slate-300 border-2 border-slate-400/30 border-opacity hover:bg-slate-800 hover:border-slate-800 w-fit"
-          href="/course"
-        >
-          Enter
-          <ArrowRight />
-        </Link>
+    <div className="text-6xl w-5/6 flex flex-col justify-start h-[calc(100vh-200px)] pt-14 gap-5 text-white">
+      <div className="text-7xl text-white">chronoX</div>
+      <div className="text-2xl text-slate-500 pb-6">
+        Timetables - but supercharged with AI
       </div>
-    </AuthCheck>
+      <Link
+        className="font-bold py-3 px-6 text-2xl flex gap-2 items-center text-slate-300 border-2 border-slate-400/30 border-opacity hover:bg-slate-800 hover:border-slate-800 w-fit"
+        href={session ? "/course" : ""}
+        onClick={() =>
+          session
+            ? {}
+            : signIn(
+                "google",
+                { callbackUrl: `http://localhost:3000` },
+                { prompt: "login" }
+              )
+        }
+      >
+        {session ? "Enter" : "Login"}
+        <ArrowRight />
+      </Link>
+    </div>
   );
 }
 
--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 export default function AuthCheck({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -11,16 +11,6 @@ export default function AuthCheck({ children }: { children: React.ReactNode }) {
   else if (status == "loading")
     return <div className="text-2xl">Loading... </div>;
   else {
-    router.push("/");
-    return (
-      <div>
-        <Link
-          className="bg-white text-2xl p-4 b-2 opacity-80 border-black rounded-xl cursor-pointer"
-          href="/login"
-        >
-          Login To Get Started
-        </Link>
-      </div>
-    );
+    redirect("/");
   }
 }
