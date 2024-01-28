1f1a6a1dd267b2fc62e2e691adb18f6ba2421cb8
Kishan1735 <gswwarriorsnba@gmail.com>
2024-01-28 08:36:09 +0530

    add profile

---
+++ app/profile/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import AuthCheck from "@/components/AuthCheck";
+import { useSession } from "next-auth/react";
+
+function Page() {
+  const { data: session, status } = useSession();
+  return (
+    <AuthCheck>
+      <div className="bg-slate-950 text-slate-300   flex flex-col space-y-4 text-xl border-[1.5px] border-slate-400 p-8">
+        <img
+          src={session?.user?.image}
+          className="h-16 w-16 mx-auto rounded-full"
+        />
+        <div className="flex space-x-4 ">
+          <div>Name:</div>
+          <div>{session?.user?.name}</div>
+        </div>
+        <div className="flex space-x-[20px]">
+          <div>Email:</div>
+          <div>{session?.user?.email}</div>
+        </div>
+        <div className="flex space-x-12">
+          <div>Id:</div>
+          <div>{session?.user?.email?.split("@")[0]}</div>
+        </div>
+      </div>
+    </AuthCheck>
+  );
+}
+
+export default Page;
